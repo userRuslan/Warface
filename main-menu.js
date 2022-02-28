f7d2642173e9294125cce6ba186157caa12f1574
RulanCuruk <curuk2003@gmail.com>
2022-02-28 16:36:17 +0300

    created functional main-menu

--- main-menu.js
+++ main-menu.js
@@ -1,10 +1,28 @@
 const mainMenu = document.querySelector(".main__menu");
 const mainItems = document.querySelectorAll(".main__item");
-console.log(mainItems);
+const cube = document.querySelector(".main__menu-logo");
+const cubeFront = document.querySelector(".menu-logo_pick");
+const cubeBottom = document.querySelector(".menu-arrow_up");
+
+cube.addEventListener("mouseover", function () {
+  cubeBottom.style.transform = "rotatex(0deg)";
+  cubeFront.style.transform = "rotatex(90deg)";
+  cube.addEventListener("mouseout", function () {
+    cubeBottom.style.transform = "";
+    cubeFront.style.transform = "";
+  });
+});
 
 document.addEventListener("scroll", function () {
-  let coord = mainMenu.getBoundingClientRect();
-  if (window.pageYOffset > coord.top + mainMenu.offsetTop) {
-  
-}
+  let coords = mainMenu.getBoundingClientRect();
+  if (window.pageYOffset > coords.top + mainMenu.offsetTop) {
+    cube.classList.add("scroll");
+  } else if (window.pageYOffset < window.pageYOffset + coords.top) {
+    cube.classList.remove("scroll");
+  }
+});
+
+cube.addEventListener("click", function (event) {
+  if (event.target != cubeBottom) return;
+  alert("hi");
 });
