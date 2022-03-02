b2bf7693b9ce2daa4f2152f18971bd4b36eba812
RulanCuruk <curuk2003@gmail.com>
2022-03-02 14:25:10 +0300

    add move main-menu

--- main-menu.js
+++ main-menu.js
@@ -3,6 +3,9 @@ const mainItems = document.querySelectorAll(".main__item");
 const cube = document.querySelector(".main__menu-logo");
 const cubeFront = document.querySelector(".menu-logo_pick");
 const cubeBottom = document.querySelector(".menu-arrow_up");
+const currentCube = document.querySelector(".cube");
+const mainLogotip = document.querySelector(".main__item:last-child");
+const mainBigElem = document.querySelector(".main__item:nth-last-child(2)");
 
 cube.addEventListener("mouseover", function () {
   cubeBottom.style.transform = "rotatex(0deg)";
@@ -17,7 +20,9 @@ document.addEventListener("scroll", function () {
   let coords = mainMenu.getBoundingClientRect();
   if (window.pageYOffset > coords.top + mainMenu.offsetTop) {
     cube.classList.add("scroll");
+    mainBigElem.classList.add("run");
   } else if (window.pageYOffset < window.pageYOffset + coords.top) {
     cube.classList.remove("scroll");
+    mainBigElem.classList.remove("run");
   }
 });
